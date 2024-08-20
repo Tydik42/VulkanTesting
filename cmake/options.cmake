fdee0608810fa7fb93aec75ae896d45bc56a9e25
Tydik <tgaraev653@gmail.com>
2024-08-20 19:37:46 +0300

    urge commit

---
+++ cmake/options.cmake
@@ -0,0 +1,34 @@
+#======================================= Information ========================================#
+
+cmake_host_system_information(RESULT vkTest_platform QUERY OS_NAME)
+string(TOUPPER ${vkTest_platform} vkTest_upper_platform)
+string(TOLOWER ${vkTest_platform} vkTest_lower_platform)
+
+#========================================== Options =========================================#
+
+option(vkTest_BUILD_DEMOS                 "Build the demos"                    ON)
+option(vkTest_COMPILE_SHADERS             "Compile the shaders"                OFF)
+
+#======================================== Directories ========================================#
+
+set(vkTest_assets_dir ${vkTest_root}/assets CACHE PATH "Path to the assets directory")
+set(vkTest_engine_dir ${vkTest_root}/engine CACHE PATH "Path to the engine directory")
+set(vkTest_thirdparty_dir ${vkTest_root}/thirdparty CACHE PATH "Path to the thirdparty directory")
+set(vkTest_demos_dir  ${vkTest_root}/demos  CACHE PATH "Path to the demo directory")
+
+#====================================== Configurations ======================================#
+
+list(APPEND CMAKE_MODULE_PATH
+        ${vkTest_assets_dir}/cmake
+        ${vkTest_engine_dir}/cmake
+        ${vkTest_demos_dir}/cmake
+        ${vkTest_thirdparty_dir}/make
+)
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+add_compile_definitions(
+        vkTest_${vkTest_upper_platform}
+        vkTest_$<UPPER_CASE:$<CONFIG>>
+)
